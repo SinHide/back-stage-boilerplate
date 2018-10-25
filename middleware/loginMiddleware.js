6194d05fd7a186418d042e1eb96476f25015d330
SinHide <yyxiejunfeng@163.com>
2018-10-25 15:36:50 +0800

    add folder & base config

---
+++ middleware/loginMiddleware.js
@@ -0,0 +1,20 @@
+const ADMIN_JSON = {
+  id: 1, name: 'Admin', authorities: 'admin'
+}
+const USER_JSON = {
+  id: 2, name: 'User', authorities: 'user'
+}
+
+module.exports = (req, res, next) => {
+  if (req.method === 'POST' && req.path === '/api/login') {
+    if (req.body.username === 'admin' && req.body.password === '123456') {
+      res.status(200).json(ADMIN_JSON)
+    } else if (req.body.username === 'user' && req.body.password === '123456') {
+      res.status(200).json(USER_JSON)
+    } else {
+      res.status(400).json({ error: 'wrong password' })
+    }
+  } else {
+    next()
+  }
+}
